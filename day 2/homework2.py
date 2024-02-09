1b6809bdea779ff28dae0c8147d0c776f25938d2
dachi67 <dachi67@users.noreply.github.com>
2024-02-09 17:57:34 +0400

    second commit

---
+++ day 2/homework2.py
@@ -0,0 +1,3 @@
+print("me " " pirvel  " " gakvetilze " "  gavicani  nika  maswavlebeli da aseve chemi razmis lideri")
+
+print("aseve  shemidzlia  vtqva  rom GOA-s akademia sauketeso programirebis akademiaa mtels saqartveloshi da darwmunebuli var iqneba N1 prohramirebis akademia mtels msoplioshi")
