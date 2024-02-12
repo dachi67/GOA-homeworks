0e1cc9bbe70ea5e8f4867c6d80dea8a94b1c68f3
dachi67 <dachi67@users.noreply.github.com>
2024-02-12 14:47:34 +0400

    second commit

---
+++ day 3/homework3.py
@@ -0,0 +1,15 @@
+deda  = 37
+mama = 44
+bebia = 60
+print(deda)
+print(mama)
+print(bebia)
+
+print("after 2 years")
+
+deda = 37+2
+mama = 44+2
+bebia = 60+2
+print(deda)
+print(mama)
+print(bebia)
