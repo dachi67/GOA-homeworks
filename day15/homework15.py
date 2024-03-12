51440ff5d3d1ecd8c38c5afed0fe8aea293e47f2
dachi67 <dachi67@users.noreply.github.com>
2024-03-12 16:53:36 +0400

    commit

---
+++ day15/homework15.py
@@ -0,0 +1,12 @@
+choose_number = int(input("print the number: "))
+
+odd =[1, 3, 5, 7, 9, 11]
+
+even = [2, 4, 6, 8, 10]
+
+if choose_number is (even):
+    print("your number is even")
+
+if choose_number is (odd):
+    print("your number is odd")
+
